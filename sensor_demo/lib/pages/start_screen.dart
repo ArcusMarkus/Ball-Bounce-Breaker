f1f9e7443130349cb34f0883ea00650a4b1856b1
David Krause <krausedl@hendrix.edu>
2026-09-22 14:44:27 -0500

    Added Buttons to start_screen.dart

--- sensor_demo/lib/pages/start_screen.dart
+++ sensor_demo/lib/pages/start_screen.dart
@@ -1,1 +1,29 @@
-// one more commit to make sure everythings working
+import 'package:flutter/material.dart';
+
+class BallBounceBreaker extends StatelessWidget {
+  const BallBounceBreaker({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('Ball Bounce Breaker')
+    ),
+    body: Column(
+      children: [
+        ElevatedButton(
+          child: const Text("Start"), 
+          onPressed: () {
+            // move us to the next screen
+          }
+        ),
+        ElevatedButton(
+          child: const Text("Quit"), 
+          onPressed: () {
+            // closes the game
+          }
+        )
+      ],
+    )
+  );
+}}
